f04929b0f42a4fb568ea4072419473738a26a5df
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2025-12-04 09:13:00 +0100

    refactor: clean up middleware code formatting for auth and guest

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,14 +1,11 @@
-import {
-  abortNavigation,
-  defineNuxtRouteMiddleware,
-} from "nuxt/app";
+import { abortNavigation, defineNuxtRouteMiddleware } from "nuxt/app";
 import { useAuthStore } from "../stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const { isAuthenticated } = useAuthStore();
-  // if (import.meta.server) return;
+  if (import.meta.server) return;
   if (isAuthenticated.customer != null) {
-    return abortNavigation(), to.path = "/" ;
-  } 
+    return abortNavigation(), (to.path = "/");
+  }
   return;
 });
