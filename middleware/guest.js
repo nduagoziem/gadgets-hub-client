827c3496c73b677824a573d1be3955c6cbae8a73
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2025-12-04 17:43:18 +0100

    refactor: commented server check in auth and guest middleware

--- middleware/guest.js
+++ middleware/guest.js
@@ -3,7 +3,7 @@ import { useAuthStore } from "../stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const { isAuthenticated } = useAuthStore();
-  if (import.meta.server) return;
+  // if (import.meta.server) return;
   if (isAuthenticated.customer != null) {
     return abortNavigation(), (to.path = "/");
   }
